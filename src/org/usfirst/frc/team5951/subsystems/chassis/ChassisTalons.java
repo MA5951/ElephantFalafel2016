f997793aa89549a6626146fb1e11c601cdd7dd04
Yair Ziv <zivyair06@gmail.com>
2016-11-21 14:11:19 +0200

    Capsulation correction

--- src/org/usfirst/frc/team5951/subsystems/chassis/ChassisTalons.java
+++ src/org/usfirst/frc/team5951/subsystems/chassis/ChassisTalons.java
@@ -5,8 +5,8 @@ import org.usfirst.frc.team5951.robot.RobotMap;
 import edu.wpi.first.wpilibj.CANTalon;
 
 public class ChassisTalons {
-	protected static CANTalon chassisLeftFront = new CANTalon(RobotMap.k_CHASSIS_LEFT_FRONT);   
-	protected static CANTalon chassisLeftRear = new CANTalon(RobotMap.k_CHASSIS_LEFT_REAR);    
-	protected static CANTalon chassisRightFront = new CANTalon(RobotMap.k_CHASSIS_RIGHT_FRONT);  
-	protected static CANTalon chassisRightRear = new CANTalon(RobotMap.k_CHASSIS_RIGHT_REAR);  
+	public static CANTalon chassisLeftFront = new CANTalon(RobotMap.k_CHASSIS_LEFT_FRONT);   
+	public static CANTalon chassisLeftRear = new CANTalon(RobotMap.k_CHASSIS_LEFT_REAR);    
+	public static CANTalon chassisRightFront = new CANTalon(RobotMap.k_CHASSIS_RIGHT_FRONT);  
+	public static CANTalon chassisRightRear = new CANTalon(RobotMap.k_CHASSIS_RIGHT_REAR);  
 }
